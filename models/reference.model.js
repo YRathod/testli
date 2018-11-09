4d5adbec636da07a9cc23f94349844e93e73db88
alpeshrpatel <alpeshrpatel@gmail.com>
2018-11-08 18:34:39 -0700

    adding text reference

--- models/reference.model.js
+++ models/reference.model.js
@@ -2,12 +2,9 @@ var mongoose = require('mongoose');
 
 // Question Schema
 var referenceModel = mongoose.Schema({
-	
-    videos:{
-        type: [String]
-    },
+
     urls:{
-        type: [String]
+        type: [String]//this can be video url , audio, image, social media url
     },
     text :{
         type: [String]
