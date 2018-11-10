2ebcda33e9bd19fe7ff639d23885ff35fc464959
alpeshrpatel <alpeshrpatel@gmail.com>
2018-11-09 23:25:22 -0700

    Adding new document

---
+++ api/logic/consumer.js
@@ -0,0 +1,5 @@
+/**
+ * 1. Consumer is responsible get questions from bank using prefer tag.
+ * 2. Create quiz based on question set
+ * 3. Publish to prefer audiance
+ */
---
+++ api/logic/publisher.js
@@ -0,0 +1,5 @@
+/**
+ * 1. Publisher is responsible for upload question using csv or some other format
+ * 2. Create quiz based on question set
+ * 3. Publish to prefer audiance
+ */
